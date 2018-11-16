ee6e3b5e314402c4cb7cbd45e904acc2f7e34d24
Ryszard <kamilszkaradnik@gmail.com>
2018-11-16 12:39:59 +0100

    Agledirection update
    Get best agle

--- shoting.py
+++ shoting.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib
 
 def shotingvertical(alfa,distance,targetheight):
     i=1
@@ -17,3 +18,41 @@ def shotingvertical(alfa,distance,targetheight):
             ontarget=2
             score = 1/(math.fabs(x-distance)+1)
     return score
+def shotinghorizontal(beta,targethorizont):
+    score=(1 / (math.fabs(beta - targethorizont) + 1))
+    return score
+
+x =shotingvertical(1/4*math.pi,100,2)
+print (x)
+
+def shotingaglevertical(distance,targetheight):
+    onpoint=0
+    lastshot =0;
+    acturalshot=0;
+    startagle=0;
+    startwector=1/4*math.pi;
+    scorelist=[]
+
+    while(1>onpoint):
+        acturalshot=shotingvertical(startagle,distance,targetheight)
+
+        if(acturalshot>lastshot):
+            startagle=startagle+startwector
+            lastshot = acturalshot
+        if(acturalshot<lastshot):
+            startwector=startwector*(-1/2)
+            startagle = startagle+startwector
+            lastshot = acturalshot
+        if(acturalshot==1):
+            onpoint=2
+        scorelist.append(acturalshot)
+    return startagle, acturalshot, scorelist
+
+x,lastshot,scorelist = shotingaglevertical(120,2)
+import matplotlib.pyplot as plt
+plt.plot(scorelist)
+plt.show()
+
+print (x)
+
+
