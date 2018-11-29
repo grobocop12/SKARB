0d8d7275167b8fb7f8f109610dec25879f98eed0
Ryszard <kamilszkaradnik@gmail.com>
2018-11-29 15:42:33 +0100

    More shots

--- shoting.py
+++ shoting.py
@@ -1,9 +1,12 @@
 import math
 import matplotlib
 import matplotlib.pyplot as plt
+import sympy
+import pandas
+import scikit
 
 def shotingvertical(alfa,distance,targetheight):
-    i=1
+    i=0
     v0=100
     g0=10
     ontarget=0
@@ -17,14 +20,104 @@ def shotingvertical(alfa,distance,targetheight):
             score = 1/(math.fabs(targetheight-y)+1)
         if(y<0):
             ontarget=2
-            score = 1/(math.fabs(x-distance)+1)
+            score = 1/(math.fabs(x-distance)+1+targetheight)
     return score
 def shotinghorizontal(beta,targethorizont):
     score=(1 / (math.fabs(beta - targethorizont) + 1))
     return score
 
-x =shotingvertical(1/4*math.pi,100,2)
-print (x)
+def shotingvertical2(alfa,distance,targetheight):
+
+    i=0
+    v0=100
+    g0=10
+    ontarget=0
+    score =0
+    height = []
+    misledistance = []
+    while(1>ontarget):
+        x=v0*i*math.cos(alfa)
+        y=v0*i*math.sin(alfa) - 1/2*i*i*g0
+        i=i+1
+        height.append(y)
+
+        misledistance.append(x)
+        if(x>distance):
+            ontarget=2
+            score = 1/(math.fabs(targetheight-y)+1)
+        if(y<0):
+            ontarget=2
+            score = 1/(math.fabs(x-distance)+1+targetheight)
+    return score, height, misledistance
+
+def shotingverticaldegrees(kat,distance,targetheight):
+    alfa = kat*math.pi/180
+    i=0
+    v0=100
+    g0=10
+    ontarget=0
+    score =0
+
+    i = sympy.Symbol('i')
+    czas = sympy.solve(v0 * i * math.sin(alfa) - (1 / 2) * i * i * g0)
+
+    i = sympy.Symbol('i')
+    czas2 = sympy.solve(v0*i*math.cos(alfa)-distance)
+
+    if(czas[1]<czas2[0]):
+        i = czas[1]
+        x2 = float(v0) * float(i) * float(math.cos(alfa))
+        score = float(1) / (math.fabs(x2 - float(distance)) + float(1) + float(targetheight))
+    else:
+        i = czas2[0]
+        y2 = float(v0) * i * float(math.sin(alfa)) - float(1/2) * i * i * float(g0)
+        score = float(1) / (math.fabs(float(targetheight) - y2) + float(1))
+
+    return score
+
+def shotingverticaldegrees2(kąt,distance,targetheight):
+    alfa = kąt*math.pi/180
+    i=0
+    v0=100
+    g0=10
+    ontarget=0
+    score =0
+    height = []
+    misledistance = []
+    while(1>ontarget):
+        x=v0*i*math.cos(alfa)
+        y=v0*i*math.sin(alfa) - (1/2)*i*i*g0
+        i=i+1
+        height.append(y)
+
+        misledistance.append(x)
+        if(x>distance):
+            ontarget=2
+            score = 1/(math.fabs(targetheight-y)+1)
+        if(y<0):
+            ontarget=2
+            score = 1/(math.fabs(x-distance)+1+targetheight)
+    i = sympy.Symbol('i')
+    czas = sympy.solve(v0 * i * math.sin(alfa) - (1 / 2) * i * i * g0)
+    print(czas)
+    i = sympy.Symbol('i')
+    czas2 = sympy.solve(v0*i*math.cos(alfa)-distance)
+    print(czas2)
+    if(czas[1]<czas2[0]):
+        i = czas[1]
+        x2 = float(v0) * float(i) * float(math.cos(alfa))
+        score = float(1) / (math.fabs(x2 - float(distance)) + float(1) + float(targetheight))
+    else:
+        i = czas2[0]
+        y2 = float(v0) * i * float(math.sin(alfa)) - float(1/2) * i * i * float(g0)
+        score = float(1) / (math.fabs(float(targetheight) - y2) + float(1))
+
+    return score, height, misledistance
+
+def shotinghorizontal(beta,targethorizont):
+    score=(1 / (math.fabs(beta - targethorizont) + 1))
+    return score
+
 
 def shotingaglevertical(distance,targetheight):
     onpoint=0
@@ -33,7 +126,7 @@ def shotingaglevertical(distance,targetheight):
     startagle=0;
     startwector=1/4*math.pi;
     scorelist=[]
-    pudło="trafiony"
+    pudlo="trafiony"
     wathdog=0
     while(1>onpoint):
         acturalshot=shotingvertical(startagle,distance,targetheight)
@@ -45,30 +138,73 @@ def shotingaglevertical(distance,targetheight):
             startwector=startwector*(-1/2)
             startagle = startagle+startwector
             lastshot = acturalshot
-        if(0.99999<acturalshot<1.0001):
+        if(0.999<acturalshot<1.001):
             onpoint=2
         if(wathdog>10000):
-            pudło="poza zasięiem"
+            pudlo="poza zasięiem"
             onpoint=2
         scorelist.append(acturalshot)
-    return startagle, acturalshot, scorelist, pudło
+    return startagle, acturalshot, scorelist, pudlo
 
-x,lastshot,scorelist, pudło = shotingaglevertical(780,2)
+def shotingaglevertical2(distance,targetheight):
+    onpoint=0
+    lastshot =0;
+    acturalshot=0;
+    startagle=0.0001;
+    startwector=20;
+    scorelist=[]
+    pudlo="trafiony"
+    wathdog=0
+    while(1>onpoint):
+        acturalshot = shotingverticaldegrees(startagle,distance,targetheight)
+        wathdog=wathdog+1
+        if(acturalshot>lastshot):
+            startagle=startagle+startwector
+            lastshot = acturalshot
+        if(acturalshot<lastshot):
+            startwector=startwector*(-2/5)
+            startagle = startagle+startwector
+            lastshot = acturalshot
+        if(0.999<acturalshot<1.001):
+            onpoint=2
+        if(wathdog>200):
+            pudlo="poza zasięiem"
+            onpoint=2
+        scorelist.append(acturalshot)
+    return startagle, acturalshot, scorelist, pudlo
+
+x,lastshot,scorelist, pudlo = shotingaglevertical(660,2)
+
 def maxdisctance():
-    range=220
+    range=800
     targeton= "trafiony"
     while (targeton== "trafiony"):
-        x,lastshot,scorelist,pudło=shotingaglevertical(range,0)
-        targeton=pudło
+        x,lastshot,scorelist,pudlo=shotingaglevertical2(range,0)
+        targeton=pudlo
         range=range+1
     return range
-max= maxdisctance()
+
+x, wysokosci, dystans = shotingvertical2(x,660,2)
+plt.plot(dystans,wysokosci)
+plt.show()
+max= 0
 plt.plot(scorelist)
 plt.show()
 
 
+
 print (x)
 print (lastshot)
-print (pudło)
+print (pudlo)
 print(max)
+agle,lastshot,scorelist, pudlo = shotingaglevertical2(460,10)
+x, wysokosci, dystans = shotingverticaldegrees2(agle,460,10)
+print(x)
+print (pudlo)
+print(agle)
+plt.plot(dystans,wysokosci)
+plt.show()
+plt.plot(scorelist)
+plt.show()
+
 
