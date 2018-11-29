0943846f44681374d87006dbd00ab1975b650438
Ryszard <kamilszkaradnik@gmail.com>
2018-11-29 17:11:36 +0100

    More updata

--- shoting.py
+++ shoting.py
@@ -3,7 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import sympy
 import pandas
-import scikit
+
 
 def shotingvertical(alfa,distance,targetheight):
     i=0
@@ -14,7 +14,7 @@ def shotingvertical(alfa,distance,targetheight):
     while(1>ontarget):
         x=v0*i*math.cos(alfa)
         y=v0*i*math.sin(alfa) - 1/2*i*i*g0
-        i=i+1
+        i=i+0.00001
         if(x>distance):
             ontarget=2
             score = 1/(math.fabs(targetheight-y)+1)
@@ -38,7 +38,7 @@ def shotingvertical2(alfa,distance,targetheight):
     while(1>ontarget):
         x=v0*i*math.cos(alfa)
         y=v0*i*math.sin(alfa) - 1/2*i*i*g0
-        i=i+1
+        i=i+0.0001
         height.append(y)
 
         misledistance.append(x)
@@ -87,7 +87,7 @@ def shotingverticaldegrees2(kąt,distance,targetheight):
     while(1>ontarget):
         x=v0*i*math.cos(alfa)
         y=v0*i*math.sin(alfa) - (1/2)*i*i*g0
-        i=i+1
+        i=i+0.0001
         height.append(y)
 
         misledistance.append(x)
@@ -184,19 +184,40 @@ def maxdisctance():
         range=range+1
     return range
 
+def shoting(alfa ,beta,distance,targetheight,airresistance,windforce):
+    alfa= alfa * math.pi / 180
+    beta = beta * math.pi / 180
+    i=0
+    v0=100
+    g0=10
+    ontarget=0
+    score =0
+    height = []
+    misledistance = []
+    horizontal=[]
+    while(1>ontarget):
+        z=v0*i*math.sin(beta) - 1/2*i*i*windforce[0]
+        x=v0*i*math.cos(alfa)*math.cos(beta) - 1/2*i*i*windforce[1]
+        y=v0*i*math.sin(alfa)*math.cos(beta) - 1/2*i*i*g0
+        i=i+0.00001
+        v0=(1-airresistance)*v0
+        height.append(y)
+        horizontal.append(z)
+        misledistance.append(x)
+        if(x>distance):
+            ontarget=2
+            score = 1/(math.fabs(targetheight-y)+1)
+        if(y<0):
+            ontarget=2
+            score = 1/(math.fabs(x-distance)+1+targetheight)
+    return score, height, misledistance,horizontal
+
 x, wysokosci, dystans = shotingvertical2(x,660,2)
 plt.plot(dystans,wysokosci)
 plt.show()
-max= 0
-plt.plot(scorelist)
-plt.show()
+print(wysokosci)
+print(dystans)
 
-
-
-print (x)
-print (lastshot)
-print (pudlo)
-print(max)
 agle,lastshot,scorelist, pudlo = shotingaglevertical2(460,10)
 x, wysokosci, dystans = shotingverticaldegrees2(agle,460,10)
 print(x)
@@ -206,5 +227,8 @@ plt.plot(dystans,wysokosci)
 plt.show()
 plt.plot(scorelist)
 plt.show()
-
-
+score, height, misledistance,horizontal = shoting(20 ,0,700,2,0.00000,[1,0])
+plt.plot(misledistance,height)
+plt.show()
+plt.plot(misledistance,horizontal)
+plt.show()
